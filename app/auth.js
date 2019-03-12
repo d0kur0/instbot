71854774ccac7184dea12ffaa3c056ad31aa8806
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-12 15:32:11 +0300

    Update colors on consol messages

--- app/auth.js
+++ app/auth.js
@@ -1,43 +1,43 @@
-const sleep = require('./sleep.js');
+const log = require('./consoleLog.js');
 const chalk = require('chalk');
 
 module.exports = async (page, authData) => {
 
-  console.log(chalk.green(`➣ Beginning authorization ...`));
+  log.header('Beginning authorization');
   await page.goto('https://instagram.com/accounts/login/');
 
-  console.log(chalk.green(`  ∟ Waiting for the user name input field to be rendered`));
+  log.info('Waiting for the user name input field to be rendered');
 
   // Await username field and filling
   await page.mainFrame().waitForSelector('input[name="username"]')
     .then(async () => {
-      console.log(chalk.blue(`  ∟ Field enter the name of the user is drawn, filling ⚝ `));
+      log.info('Field enter the name of the user is drawn, filling');
 
       await page.focus('input[name="username"]');
       await page.keyboard.type(authData.username);
     })
     .catch(() => {
-      console.log(chalk.red(`  ∟ User name input field is not render`))
+      log.error('Field enter the name of the user is drawn, filling');
     });
 
   // Await password field and filling
   await page.mainFrame().waitForSelector('input[name="password"]')
     .then(async () => {
-      console.log(chalk.blue(`  ∟ Field enter the password of the user is drawn, filling ⚝ `));
+      log.success('Field enter the password of the user is drawn, filling');
 
       await page.focus('input[name="password"]');
       await page.keyboard.type(authData.password);
     })
     .catch(() => {
-      console.log(chalk.red(`  ∟ Password input field is not render`))
+      log.error('Password input field is not render');
     });
 
   await page.click('button[type="submit"]')
     .then(() => {
-      console.log(chalk.green(`  ∟ User data entry fields are filled in, emulating sending the form`));
+      log.success('User data entry fields are filled in, emulating sending the form');
     })
     .catch(() => {
-      console.log(chalk.red(`  ∟ Error sending authorization form`));
+      log.error('Error sending authorization form');
     });
 
   // Await auth request
@@ -45,27 +45,27 @@ module.exports = async (page, authData) => {
 
   await page.goto('https://instagram.com')
     .then(() => {
-      console.log(chalk.green(`  ∟ Open instagram.com to check authorization`));
+      log.success('Open instagram.com to check authorization');
     })
     .catch(() => {
-      console.log(chalk.red(`  ∟ Could not open instagram.com`));
+      log.error('Could not open instagram.com');
     });
 
   await page.mainFrame().waitForSelector('div.mt3GC > button.aOOlW.HoLwm')
     .then(() => {
-      console.log(chalk.green(`  ∟ The selector div.mt3GC > button.aOOlW.HoLwm appeared in DOM`));
+      log.success('The selector div.mt3GC > button.aOOlW.HoLwm appeared in DOM');
     })
     .catch(() => {
-      console.log(chalk.red(`  ∟ Failed to find the selector div.mt3GC > button.aOOlW.HoLwm in DOM`));
+      log.error('Failed to find the selector div.mt3GC > button.aOOlW.HoLwm in DOM');
     });
 
   const authResponse = await page.evaluate(() => Boolean(document.querySelector('div.mt3GC > button.aOOlW.HoLwm')));
 
   if (!authResponse) {
-    console.log(chalk.red(`  ∟ Authorization failed, something went wrong ⚝ `));
+    log.error('Authorization failed, something went wrong')
     return false;
   }
 
-  console.log(chalk.green.bold(`  ∟ Successfully logged in ❤ `));
+  log.success('Successfully logged in ❤');
   return true;
 };
--- app/consoleLog.js
+++ app/consoleLog.js
@@ -2,18 +2,18 @@ const chalk = require('chalk');
 
 module.exports = {
   header (message) {
-    console.log(chalk.green(`\r\n➣ ${message} `))
+    console.log(chalk.cyan.bold(`\r\n➣ ${message} `))
   },
 
   info (message) {
-    console.log(chalk.blue(`  ∟ ${message} `))
+    console.log(chalk.gray.bold(`  ⮡ ${message} `))
   },
 
   success (message) {
-    console.log(chalk.green(`  ∟ ${message} `))
+    console.log(chalk.yellow.bold(`  ⮡ ${message} `))
   },
 
   error (message) {
-    console.log(chalk.red(`  ∟ ${message} `))
+    console.log(chalk.red.bold(`  ⮡ ྾ ${message} `))
   }
 };
