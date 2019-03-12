2fe86055bd34c0d2c93bbe9cc6d4eaaf094ca787
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-12 17:18:52 +0300

    Bot write random comment in base

---
+++ app/messages.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "Awesome! <3",
+  "Красиво!"
+];
---
+++ app/setComment.js
@@ -0,0 +1,17 @@
+const log = require('./consoleLog.js');
+const messages = require('./messages.js');
+
+module.exports = async (page) => {
+  await page.mainFrame().waitForSelector('textarea.Ypffh')
+    .then(async () => {
+      let message = messages[Math.floor(Math.random() * messages.length)];
+
+      await page.focus('textarea.Ypffh');
+      await page.keyboard.type(message);
+      await page.keyboard.press('Enter');
+      await page.waitForRequest(request => request.method() === 'POST');
+    })
+    .catch(() => {
+      log.error('The input field of the comment is not found');
+    });
+};
