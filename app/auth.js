478e88e862ef8a63384c80a56c76f932f7d36603
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-12 13:53:13 +0300

    Event-base logic, rewrite comments to English lang, steps bot as modules, fix errors on subscribe, new options

--- app/auth.js
+++ app/auth.js
@@ -3,40 +3,69 @@ const chalk = require('chalk');
 
 module.exports = async (page, authData) => {
 
-  console.log(chalk.green(`➣ Начинаю авторизацию ...`));
-
+  console.log(chalk.green(`➣ Beginning authorization ...`));
   await page.goto('https://instagram.com/accounts/login/');
-  await sleep(1000);
 
-  console.log(chalk.green(`  ∟ Эмулирую ввод данных и пытаюсь зайти`));
-  await page.focus('input[name="username"]');
-  await page.keyboard.type(authData.username);
+  console.log(chalk.green(`  ∟ Waiting for the user name input field to be rendered`));
 
-  await page.focus('input[name="password"]');
-  await page.keyboard.type(authData.password);
+  // Await username field and filling
+  await page.mainFrame().waitForSelector('input[name="username"]')
+    .then(async () => {
+      console.log(chalk.blue(`  ∟ Field enter the name of the user is drawn, filling ⚝ `));
 
-  await page.click('button[type="submit"]');
+      await page.focus('input[name="username"]');
+      await page.keyboard.type(authData.username);
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ User name input field is not render`))
+    });
 
-  console.log(chalk.green(`  ∟ Запрос прошёл, проверяю залогинен ли`));
+  // Await password field and filling
+  await page.mainFrame().waitForSelector('input[name="password"]')
+    .then(async () => {
+      console.log(chalk.blue(`  ∟ Field enter the password of the user is drawn, filling ⚝ `));
 
-  await sleep(1000);
-  await page.goto('https://instagram.com');
+      await page.focus('input[name="password"]');
+      await page.keyboard.type(authData.password);
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ Password input field is not render`))
+    });
 
-  const authResponse = await page.evaluate(() => {
-    let issetNotificationButton = document.querySelector('div.mt3GC > button.aOOlW.HoLwm');
+  await page.click('button[type="submit"]')
+    .then(() => {
+      console.log(chalk.green(`  ∟ User data entry fields are filled in, emulating sending the form`));
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ Error sending authorization form`));
+    });
 
-    if (issetNotificationButton !== null) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  // Await auth request
+  await page.waitForResponse('https://www.instagram.com/accounts/login/ajax/');
+
+  await page.goto('https://instagram.com')
+    .then(() => {
+      console.log(chalk.green(`  ∟ Open instagram.com to check authorization`));
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ Could not open instagram.com`));
+    });
+
+  await page.mainFrame().waitForSelector('div.mt3GC > button.aOOlW.HoLwm')
+    .then(() => {
+      console.log(chalk.green(`  ∟ The selector div.mt3GC > button.aOOlW.HoLwm appeared in DOM`));
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ Failed to find the selector div.mt3GC > button.aOOlW.HoLwm in DOM`));
+    });
+
+  const authResponse = await page.evaluate(() => Boolean(document.querySelector('div.mt3GC > button.aOOlW.HoLwm')));
 
   if (!authResponse) {
-    console.log(chalk.red(`  ∟ Кажется авторизация не удалась, что-то пошло не так ⚝ `));
+    console.log(chalk.red(`  ∟ Authorization failed, something went wrong ⚝ `));
     return false;
   }
 
-  console.log(chalk.green.bold(`  ∟ Успешно авторизовались ❤ `));
+  console.log(chalk.green.bold(`  ∟ Successfully logged in ❤ `));
   return true;
 };
--- app/loop.js
+++ app/loop.js
@@ -1,125 +1,107 @@
 const sleep = require('./sleep.js');
 const chalk = require('chalk');
+const setLike = require('./setLike');
+const subscribe = require('./subscribe.js');
 
 module.exports = async (options, page) => {
-  console.log(chalk.green(`\r\n➣ Открываю страницу постов хештега...`));
+  console.log(chalk.green(`\r\n➣ The opening page of the hashtag posts...`));
   await page.goto(options.hashTagUri);
-  await sleep(500);
 
-  console.log(chalk.green(`  ∟ Жду появления в DOM дереве селектора .EZdmt`));
+  console.log(chalk.green(`  ∟ Waiting for the render mesh posts`));
   await page.mainFrame().waitForSelector('.EZdmt')
-      .then(() => console.log(chalk.blue(`  ∟ Селектор найден`)));
+    .then(() => {
+      console.log(chalk.blue(`  ∟ Posts successfully rendered`))
+    })
+    .catch(() => {
+      console.log(chalk.red(`  ∟ Posts could not rendered`))
+    });
 
   const renderResponse = await page.evaluate((removePopular) => {
-    let posts = document.querySelectorAll('.v1Nh3.kIKUG._bz0w');
-
     if (removePopular) {
       document.querySelector('.EZdmt').remove();
     }
 
-    return Boolean(posts.length);
+    return Boolean(document.querySelectorAll('.v1Nh3.kIKUG._bz0w').length);
   }, options.bot.removePopular);
 
   if (!renderResponse) {
-    console.log(chalk.red(`  ∟ Не смог найти ни одного поста ⚝ `));
+    console.log(chalk.red(`  ∟ No posts found in DOM ⚝ `));
     return false;
   }
 
-  console.log(chalk.green(`  ∟ Посты хештега успешно отрендерились`));
-
-  if (options.bot.removePopular) {
-    console.log(chalk.green(`  ∟ Удалил раздел популярных постов`));
-  }
-
   let recursionLoop = async () => {
-    console.log(chalk.green(`  ∟ Жду появления в DOM дереве селектора .v1Nh3.kIKUG._bz0w > a`));
+
     await page.mainFrame().waitForSelector('.v1Nh3.kIKUG._bz0w > a')
-        .then(() => console.log(chalk.blue(`  ∟ Селектор найден ⚝ `)))
-        .catch(() => console.log(chalk.red(`  ∟ Не смог найти селектор`)));
+      .then(() => {
+        console.log(chalk.blue(`  ∟ Photos were rendering ⚝ `))
+      })
+      .catch(() => {
+        console.log(chalk.red(`  ∟ Not a single photo doesn't render`))
+      });
 
     let postCount = await page.$$('.v1Nh3.kIKUG._bz0w > a');
-    let iteration = 0;
     postCount = postCount.length;
 
+    let iteration = 0;
     while (postCount >= 0) {
-      console.log(chalk.green(`  ∟ Жду открытия окна фото`));
       await page.$eval('.v1Nh3.kIKUG._bz0w > a', el => el.click())
-          .then(() => console.log(chalk.blue(`  ∟ Успешно открыто ⚝ `)))
-          .catch(() => console.log(chalk.red(`  ∟ Не смог открыть`)));;
-
-      /**
-       * setLike
-       */
-      if (options.bot.setLike) {
-        console.log(chalk.green(`  ∟ Жду появления в DOM дереве кнопки лайка`));
-        await page.mainFrame().waitForSelector('.dCJp8.afkep.coreSpriteHeartOpen._0mzm-')
-            .then(() => console.log(chalk.blue(`  ∟ Успешно нашёл, ставлю лайк ⚝ `)))
-            .catch(() => console.log(chalk.red(`  ∟ Не смог найти`)));
-
-        let likeResult = await page.$eval('.dCJp8.afkep.coreSpriteHeartOpen._0mzm-', (el) => {
-          let child = el.childNodes[0];
-
-          if (!child.classList.contains('glyphsSpriteHeart__filled__24__red_5')) {
-            el.click();
-            return true;
-          } else {
-            return false;
-          }
+        .then(() => {
+          console.log(chalk.green(`  ∟ Successfully opened the photo (#${iteration}) ⚝ `))
+        })
+        .catch(() => {
+          console.log(chalk.red(`  ∟ Couldn't open the photo (#${iteration})`))
         });
 
-        if (likeResult) {
-          console.log(chalk.blue.bold(`  ∟ Лайк успешно поставлен`));
-        } else {
-          console.log(chalk.blue.bold(`  ∟ Лайк уже стоит, иду дальше`));
-        }
+      if (options.bot.setLike) {
+        setLike(page);
       }
 
-      /**
-       * Subscribe
-       */
+      await sleep(options.bot.delayBeforeLike);
+
       if (options.bot.subscribe) {
-        try {
-          console.log(chalk.green(`  ∟ Жду появления в DOM дереве кнопки подписки`));
-          await page.mainFrame().waitForSelector('.oW_lN._0mzm-.sqdOP.yWX7d')
-              .then(() => console.log(chalk.blue(`  ∟ Успешно нашёл, подписываюсь`)))
-              .catch(() => console.log(chalk.red(`  ∟ Не смог найти`)));
-
-          let subsResult = await page.$eval('.oW_lN._0mzm-.sqdOP.yWX7d', (el) => {
-            el.click();
-
-            return true;
-          });
-
-          if (subsResult) {
-            console.log(chalk.blue.bold(`  ∟ Успешно подписался ⚝ `));
-          } else {
-            console.log(chalk.blue.bold(`  ∟ Не удалось подписаться`));
-          }
-
-        } catch (exception) {
-          continue;
-        }
+        subscribe(page);
       }
 
-      console.log(chalk.green(`  ∟ Жду появления в DOM кнопки закрытия окна`));
+      await sleep(options.bot.delayBeforeSubscribe);
+
       await page.mainFrame().waitForSelector('button.ckWGn')
-          .then(() => console.log(chalk.blue(`  ∟ Успешно нашёл, закрываю`)))
-          .catch(() => console.log(chalk.red(`  ∟ Не смог найти`)));;
+        .then(() => {
+          console.log(chalk.blue(`  ∟ Found the button to close the photo window`))
+        })
+        .catch(() => {
+          console.log(chalk.red(`  ∟ Photo window close button not found`))
+        });
 
-      await page.click('button.ckWGn');
+      await page.click('button.ckWGn')
+        .then(() => {
+          console.log(chalk.green(`  ∟ Successfully closed the window photo`))
+        })
+        .catch(() => {
+          console.log(chalk.red(`  ∟ Failed to close photo window`))
+        });
 
-      console.log(chalk.green(`  ∟ Удаляю обработанный элемент из DOM`));
       await page.$eval('.v1Nh3.kIKUG._bz0w > a', el => el.remove())
-          .then(() => console.log(chalk.blue(`  ∟ Успешно удалил, иду дальше ⚝ `)))
-          .catch(() => console.log(chalk.red(`  ∟ Не смог удалить`)));;
+        .then(() => {
+          console.log(chalk.green(`  ∟ Successfully deleted the photo element from the DOM ⚝ `))
+        })
+        .catch(() => {
+          console.log(chalk.red(`  ∟ Unable to remove item from DOM`))
+        });
 
-      await sleep(options.bot.delay);
+      await sleep(options.bot.beforeIterationDelay);
 
       iteration++;
 
       if (postCount === iteration) {
-        console.log(chalk.green(`  ∟ Гружу новые посты`));
-        await page.$eval('html', el => window.scrollTo(0, document.body.scrollHeight));
+        await page.$eval('html', () => window.scrollTo(0, document.body.scrollHeight))
+          .then(async () => {
+            console.log(chalk.green(`  ∟ The cycle is over, start a new one ⚝ `))
+          })
+          .catch(() => {
+            console.log(chalk.red(`  ∟ Failed to scroll page, new posts not received`))
+          });
+
+
         await recursionLoop();
       }
     }
