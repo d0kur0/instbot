b57a5684994cfe47ce218bf7692c057ede9f9454
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-13 14:10:36 +0300

    Переписал комментарии в консоль на русский язык, изменена структура лог-сообщений в консоль

--- app/closeRemove.js
+++ app/closeRemove.js
@@ -1,19 +1,27 @@
 const log = require('./consoleLog.js');
 
 module.exports = async (page) => {
-  await page.click('button.ckWGn')
+  await page.mainFrame().waitForSelector('button.ckWGn')
     .then(() => {
-      log.success('Successfully closed the window photo');
+      log.info('Найдена кнопка закрытия поп-ап поста');
     })
     .catch(() => {
-      log.error('Failed to close photo window');
+      log.error('Не удалось найти кнопку закрытия поп-ап поста');
+    });
+
+  await page.click('button.ckWGn')
+    .then(() => {
+      log.success('Поп-ап поста успешно закрыт');
+    })
+    .catch(() => {
+      log.error('Не удалось закрыть поп-ап поста');
     });
 
   await page.$eval('.v1Nh3.kIKUG._bz0w > a', el => el.remove())
     .then(() => {
-      log.success('Successfully deleted the photo element from the DOM');
+      log.success('Элемент поста успешно удалён из DOM');
     })
     .catch(() => {
-      log.error('Unable to remove item from DOM');
+      log.error('Не удалось удалить элемент поста из DOMы');
     });
 };
