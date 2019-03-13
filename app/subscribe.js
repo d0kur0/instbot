4a2d4be20f1bb55e3cde98058f451f8b98601a7b
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-13 10:23:38 +0300

    Not await sleep if subscribe button not exists

--- app/subscribe.js
+++ app/subscribe.js
@@ -1,6 +1,7 @@
 const log = require('./consoleLog.js');
+const sleep = require('./sleep.js');
 
-module.exports = async (page) => {
+module.exports = async (page, options) => {
   let existsBtn = await page.$('div.PQo_0 > div.bY2yH > button:not(._8A5w5)');
 
   if (existsBtn) {
@@ -11,5 +12,7 @@ module.exports = async (page) => {
       .catch(() => {
         log.error('A subscription button was found but could not be signed');
       });
+
+    await sleep(options.bot.delayBeforeSubscribe);
   }
 };
