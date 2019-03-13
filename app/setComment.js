7c7f0936fab25da6e4de3b75f918ed3d3118bd04
nikolaj.kozlov <dev.dokuro@gmail.com>
2019-03-13 14:44:41 +0300

    Логи отправки комментария

--- app/setComment.js
+++ app/setComment.js
@@ -8,13 +8,23 @@ module.exports = async (page) => {
       let message = messages[Math.floor(Math.random() * messages.length)];
 
       await page.focus('textarea.Ypffh');
-      await page.keyboard.type(message);
+
+      await page.keyboard.type(message)
+        .then(() => {
+          log.success('Заполнил поле ввода комментарий');
+        })
+        .catch(() => {
+          log.error('Не удалось ввести текст комментария в поле ввода');
+        });;
 
       const pendingXHR = new PendingXHR(page);
-      await page.keyboard.press('Enter');
+      await page.keyboard.press('Enter')
+        .then(() => {
+          log.success('Эмуляция отправки комментария');
+        });
       await pendingXHR.waitForAllXhrFinished()
         .then(() => {
-          log.info('Ожидание завершения всех XHR запросов');
+          log.info('Все XHR запросы завершены');
         });
     })
     .catch(() => {
