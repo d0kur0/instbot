fb38448be9602b45f0a40cfef6699f3bf7f447c2
dokuro <dev.dokuro@gmail.com>
2019-03-16 13:20:40 +0300

    Отдельная модульная конфигурация

---
+++ app/configs/botSettings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  isRemovePopular: true,
+  isSubscribe: true,
+  isComment: true,
+
+  delayBeforeComment: 1000,
+  delayBeforeIteration: 15000,
+  delayBeforeLike: 1000,
+  delayBeforeSubscribe: 1000
+};
