5084f4008ef3134127ee94c6ec75874a0a0fb3a2
dokuro <dev.dokuro@gmail.com>
2019-03-16 13:19:40 +0300

    Перемещение в папку модулей

---
+++ app/modules/consoleLog.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+
+module.exports = {
+  header (message) {
+    console.log(chalk.cyan.bold(`\r\n➣ ${message} `))
+  },
+
+  info (message) {
+    console.log(chalk.gray.bold(`  ⮡ ${message} `))
+  },
+
+  success (message) {
+    console.log(chalk.yellow.bold(`  ⮡ ${message} `))
+  },
+
+  error (message) {
+    console.log(chalk.red.bold(`  ⮡ ྾ ${message} `))
+  }
+};
