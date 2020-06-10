f38a5a4d3f3bec3ccc71a828a5c06a3c8f8355bc
d0kur0 <dev.dokuro@gmail.com>
2020-06-11 00:06:50 +0300

    Prettier result

--- app/process.js
+++ app/process.js
@@ -1,19 +1,22 @@
 global._app = `${__dirname}`;
 
-const puppeteer       = require('puppeteer');
+const puppeteer = require("puppeteer");
 const browserSettings = require(`${_app}/configs/browserSettings`);
-const auth            = require(`${_app}/modules/auth`);
-const loop            = require(`${_app}/loop.js`);
+const auth = require(`${_app}/modules/auth`);
+const loop = require(`${_app}/loop.js`);
+const path = require("path");
+
+global._dbPath = path.join(__dirname, "./limits.json");
 
 (async () => {
   const browser = await puppeteer.launch(browserSettings);
-  const page    = await browser.newPage();
+  const page = await browser.newPage();
 
-  if (!await auth(page)) {
+  if (!(await auth(page))) {
     return await browser.close();
   }
 
-  if (!await loop(page)) {
+  if (!(await loop(page))) {
     return await browser.close();
   }
 })();
