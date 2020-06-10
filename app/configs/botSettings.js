66797e21c1c16984054c7ef4996b1b90e4a26e07
d0kur0 <dev.dokuro@gmail.com>
2020-06-11 00:02:35 +0300

    Add limits configs, change delays, add comments

--- app/configs/botSettings.js
+++ app/configs/botSettings.js
@@ -1,10 +1,17 @@
 module.exports = {
+  // Enabled modules
   isRemovePopular: true,
   isSubscribe: true,
   isComment: true,
 
-  delayBeforeComment: 1000,
-  delayBeforeIteration: 15000,
-  delayBeforeLike: 1000,
-  delayBeforeSubscribe: 1000
+  // Delays
+  delayBeforeComment: 60000,
+  delayBeforeIteration: 60000,
+  delayBeforeLike: 60000,
+  delayBeforeSubscribe: 60000,
+
+  // Limits
+  maxLikesPeerDay: 180,
+  maxSubscribesPeerDay: 90,
+  maxCommentsPeerDay: 190,
 };
