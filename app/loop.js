93548b6b88950e9b1317cd4237e612f4fccbdb06
d0kur0 <dev.dokuro@gmail.com>
2020-10-11 23:05:48 +0300

    Change changed condition, reformat

--- app/loop.js
+++ app/loop.js
@@ -1,58 +1,66 @@
-const sleep        = require(`${_app}/modules/sleep`);
-const log          = require(`${_app}/modules/consoleLog`);
-const setLike      = require(`${_app}/modules/setLike`);
-const cleanPost    = require(`${_app}/modules/cleanPost`);
-const timestamp    = require(`${_app}/modules/getTimestamp`);
+const sleep = require(`${_app}/modules/sleep`);
+const log = require(`${_app}/modules/consoleLog`);
+const setLike = require(`${_app}/modules/setLike`);
+const cleanPost = require(`${_app}/modules/cleanPost`);
+const timestamp = require(`${_app}/modules/getTimestamp`);
 const setSubscribe = require(`${_app}/modules/setSubscribe`);
-const setComment   = require(`${_app}/modules/setComment`);
-const botSettings  = require(`${_app}/configs/botSettings`);
-const hashTags     = require(`${_app}/configs/hashTagsList`);
+const setComment = require(`${_app}/modules/setComment`);
+const botSettings = require(`${_app}/configs/botSettings`);
+const hashTags = require(`${_app}/configs/hashTagsList`);
 
-module.exports = async (page) => {
+module.exports = async page => {
+  const SELECTOR_POPULAR_POSTS = ".EZdmt";
+  const SELECTOR_OPEN_POST = ".v1Nh3.kIKUG._bz0w > a";
+  const INIT_TIMESTAMP = timestamp();
 
-  const SELECTOR_POPULAR_POSTS = '.EZdmt';
-  const SELECTOR_OPEN_POST     = '.v1Nh3.kIKUG._bz0w > a';
-  const INIT_TIMESTAMP         = timestamp();
+  const openHashTag = async hashTag => {
+    await page
+      .goto(`https://www.instagram.com/explore/${hashTag}`)
+      .then(() => {
+        log.header("Открытие страницы хештега");
+      })
+      .catch(() => {
+        log.error("Не удалось открыть страницу хештега");
+      });
 
-  const openHashTag = async (hashTag) => {
-    await page.goto(`https://www.instagram.com/explore/${hashTag}`)
-        .then(() => {
-          log.header('Открытие страницы хештега');
-        })
-        .catch(() => {
-          log.error('Не удалось открыть страницу хештега');
-        });
+    await page
+      .mainFrame()
+      .waitForSelector(SELECTOR_POPULAR_POSTS)
+      .then(() => {
+        log.success("Посты успешно отрендерились");
+      })
+      .catch(() => {
+        log.error("Рендеринг постов завершился ошибкой");
+      });
 
-    await page.mainFrame().waitForSelector(SELECTOR_POPULAR_POSTS)
-        .then(() => {
-          log.success('Посты успешно отрендерились');
-        })
-        .catch(() => {
-          log.error('Рендеринг постов завершился ошибкой');
-        });
-
-    await page.evaluate((isRemovePopular, SELECTOR_POPULAR_POSTS) => {
-      if (isRemovePopular) {
-        document.querySelector(SELECTOR_POPULAR_POSTS).remove();
-      }}, botSettings.isRemovePopular, SELECTOR_POPULAR_POSTS);
+    await page.evaluate(
+      (isRemovePopular, SELECTOR_POPULAR_POSTS) => {
+        if (isRemovePopular) {
+          document.querySelector(SELECTOR_POPULAR_POSTS).remove();
+        }
+      },
+      botSettings.isRemovePopular,
+      SELECTOR_POPULAR_POSTS
+    );
   };
 
   await openHashTag(hashTags.getRandomTag());
 
   const recursionLoop = async () => {
-
-    if (INIT_TIMESTAMP + hashTags.changeAfter < timestamp()) {
+    if (INIT_TIMESTAMP + botSettings.changeAfter < timestamp()) {
       await openHashTag(hashTags.getRandomTag())
-          .then(() => log.success('Обновлён тег для работы'))
-          .catch(() => log.error('Не удалось обновить тег'));
+        .then(() => log.success("Обновлён тег для работы"))
+        .catch(() => log.error("Не удалось обновить тег"));
     }
 
-    await page.mainFrame().waitForSelector(SELECTOR_OPEN_POST)
+    await page
+      .mainFrame()
+      .waitForSelector(SELECTOR_OPEN_POST)
       .then(() => {
-        log.success('Кнопка открытия поп-ап поста успешно отрендерена');
+        log.success("Кнопка открытия поп-ап поста успешно отрендерена");
       })
       .catch(() => {
-        log.error('Кнопка открытия поп-ап поста не была отрендерена');
+        log.error("Кнопка открытия поп-ап поста не была отрендерена");
       });
 
     let postCount = await page.$$(SELECTOR_OPEN_POST);
@@ -63,7 +71,8 @@ module.exports = async (page) => {
       log.header(`Начало итерации: #${iteration}`);
 
       try {
-        await page.$eval(SELECTOR_OPEN_POST, el => el.click())
+        await page
+          .$eval(SELECTOR_OPEN_POST, el => el.click())
           .then(() => {
             log.success(`Поп-ап поста успешно открыт (#${iteration})`);
           })
@@ -83,7 +92,9 @@ module.exports = async (page) => {
             await sleep(botSettings.delayBeforeComment);
           }
         } else {
-          log.error('Лайк уже стоит, пропускаю подписку и написание комментария');
+          log.error(
+            "Лайк уже стоит, пропускаю подписку и написание комментария"
+          );
         }
 
         await cleanPost(page);
@@ -92,12 +103,17 @@ module.exports = async (page) => {
         iteration++;
 
         if (postCount === iteration) {
-          await page.$eval('html', () => window.scrollTo(0, document.body.scrollHeight))
+          await page
+            .$eval("html", () => window.scrollTo(0, document.body.scrollHeight))
             .then(async () => {
-              log.header('Цикл завершился, скролл страницы вниз для загрузки новых постов');
+              log.header(
+                "Цикл завершился, скролл страницы вниз для загрузки новых постов"
+              );
             })
             .catch(() => {
-              log.error('Не удалось проскроллить страницу для получения новых постов');
+              log.error(
+                "Не удалось проскроллить страницу для получения новых постов"
+              );
             });
 
           await recursionLoop();
