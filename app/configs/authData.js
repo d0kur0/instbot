65adc895e605d9efef07cbee043b788efd4a47ab
d0kur0 <dev.dokuro@gmail.com>
2020-10-21 02:39:20 +0300

    Remove old useless code

--- app/configs/authData.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("./dataFiles/auth.json");
--- app/configs/hashTagsList.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  list: require("./dataFiles/hashTags.json"),
-
-  getRandomTag() {
-    return this.list[Math.floor(Math.random() * this.list.length)];
-  },
-};
