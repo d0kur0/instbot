e1488d7d1a5202155293c78b2dc8b7f05401e618
d0kur0 <dev.dokuro@gmail.com>
2021-01-03 23:06:48 +0300

    Move processes in outside module, add unsubscribe mode

--- app/index.js
+++ app/index.js
@@ -1,53 +1,17 @@
 import auth from "./steps/signIn.js";
 import { writeError } from "./utils/logger.js";
-import getTimeStamp from "./utils/getTimeStamp.js";
-import settings from "./settings.json";
-import openSource from "./steps/openSource.js";
-import browserInstance from "./browserInstance.js";
-import openPost from "./steps/openPost.js";
-import getPostsCount from "./steps/getPostsCount.js";
-import clickLike from "./steps/clickLike.js";
-import clickSubscribe from "./steps/clickSubscribe.js";
-import writeComment from "./steps/writeComment.js";
-import sleep from "./utils/sleep.js";
-import removeUselessPost from "./steps/removeUselessPost.js";
-import scrollToBottom from "./steps/scrollToBottom.js";
-import sample from "lodash.sample";
+import { defaultProcess, unsubscribeProcess } from "./processes.js";
 
-let lastIterationChange = getTimeStamp();
-let currentSource = sample(settings.sourcesList);
-
-const isExpireSource = () => lastIterationChange + settings.delays.afterChangeSource < getTimeStamp();
-
-const process = async () => {
-  if (isExpireSource()) {
-    currentSource = sample(settings.sourcesList);
-    lastIterationChange = getTimeStamp();
-  }
-
-  await openSource(currentSource);
-
-  while (true) {
-    if (isExpireSource()) break;
-
-    await openPost();
-
-    await clickLike().catch(() => writeError("Ошибка установки лайка"));
-    await clickSubscribe().catch(() => writeError("Ошибка установки подписки"));
-    await writeComment().catch(() => writeError("Ошибка написания комментария"));
-
-    await removeUselessPost();
-    (await getPostsCount()) || (await scrollToBottom());
-
-    await sleep(settings.delays.afterEndIteration);
-  }
-
-  await process();
-};
+const [, , mode] = process.argv;
 
 try {
   await auth();
-  await process();
+
+  if (mode === "--unsubscribe") {
+    await unsubscribeProcess();
+  } else {
+    await defaultProcess();
+  }
 } catch (error) {
   await writeError(error.message);
 }
